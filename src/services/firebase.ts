f2924c3d9e220dead806b938c5fa7127fd9733de
Alexis Pavon <themaster034@gmail.com>
2023-01-30 00:39:34 -0300

    change config firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCZ1JcR474982FrlduXTVO7q74cHZnor8",
-  authDomain: "dashboard-lagom.firebaseapp.com",
-  projectId: "dashboard-lagom",
-  storageBucket: "dashboard-lagom.appspot.com",
-  messagingSenderId: "460736580067",
-  appId: "1:460736580067:web:2a0eddce984a44d88038d6"
+  apiKey: "AIzaSyDi1bw8MjAt6Pia3Fcm835TfN7MBo4b0i4",
+  authDomain: "marketing-app-9895f.firebaseapp.com",
+  projectId: "marketing-app-9895f",
+  storageBucket: "marketing-app-9895f.appspot.com",
+  messagingSenderId: "678403164562",
+  appId: "1:678403164562:web:26c6ef908eb936c08cd94b"
 };
 
 export const FirebaseApp  = initializeApp(firebaseConfig);
-export const FirebaseAuth = getAuth( FirebaseApp );
-export const FirebaseDB   = getFirestore( FirebaseApp );
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB   = getFirestore(FirebaseApp);
