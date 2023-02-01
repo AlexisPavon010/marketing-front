529e0e1e25cca9ad52291798163feeff03d3aca0
Alexis Pavon <themaster034@gmail.com>
2023-02-01 13:29:25 -0300

    add find users to db

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
-import { collection, doc, getDocs, setDoc } from "firebase/firestore/lite";
+import { collection, doc, getDocs, setDoc, updateDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../services/firebase";
 
 export const FindUserToDb = async (uid: string) => {
@@ -15,9 +15,30 @@ export const FindUserToDb = async (uid: string) => {
   return user[0]?.role;
 }
 
-export const saveUserToDb = async (uid: string) => {
+export const findAllUsersToDb = async () => {
+  const usersCollectionRef = collection(FirebaseDB, 'users');
+  const docs = await getDocs(usersCollectionRef);
+
+  const users: any = [];
+  docs.forEach(doc => {
+    users.push({ id: doc.id, ...doc.data() });
+  });
+
+  return users;
+}
+
+export const handleChangeRole = async (uid: string, newRole: string) => {
+  const newDoc = doc(collection(FirebaseDB, `users/${uid}/roles`));
+  await updateDoc(newDoc, {
+    role: newRole
+  })
+}
+
+export const saveUserToDb = async ({ uid, email, displayName }: any) => {
   const newDoc = doc(collection(FirebaseDB, `users/${uid}/roles`));
   await setDoc(newDoc, {
-    role: 'client'
+    role: 'client',
+    email: email,
+    username: displayName
   });
 }
