af2f322f54bb23df15db68a8c623806ebe0f10f0
Alexis Pavon <themaster034@gmail.com>
2023-02-02 10:59:07 -0300

    change firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDi1bw8MjAt6Pia3Fcm835TfN7MBo4b0i4",
-  authDomain: "marketing-app-9895f.firebaseapp.com",
-  projectId: "marketing-app-9895f",
-  storageBucket: "marketing-app-9895f.appspot.com",
-  messagingSenderId: "678403164562",
-  appId: "1:678403164562:web:26c6ef908eb936c08cd94b"
+  apiKey: "AIzaSyBDWcCtUab9Ev33lSf-TkgM9xUNOZtxxcI",
+  authDomain: "marketing-badbf.firebaseapp.com",
+  projectId: "marketing-badbf",
+  storageBucket: "marketing-badbf.appspot.com",
+  messagingSenderId: "851900811468",
+  appId: "1:851900811468:web:fd339eefb64076628f9a69"
 };
 
 export const FirebaseApp  = initializeApp(firebaseConfig);
