c2bd3db76753e97968f2698c97332473f489ce1b
Alexis Pavon <themaster034@gmail.com>
2023-02-02 18:28:25 -0300

    add categories collapse

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,32 +1,54 @@
-import { Card, Col, Row } from 'antd'
-import { Link } from 'react-router-dom'
-import { CATEGORIES } from '../constans'
+import { Card, Typography } from 'antd'
+import { AiOutlineUser } from 'react-icons/ai'
+import { useSelector } from 'react-redux'
+
+import { CategoriesCard } from '../components/Categories'
+import { CategoriesCollapse } from '../components/CategoriesCollapse';
+
+const { Title, Paragraph } = Typography;
 
 export const Home = () => {
+  const { displayName, email } = useSelector((state: any) => state.auth)
+
   return (
-    <Card>
-      <Row gutter={[42, 32]}>
-        {
-          CATEGORIES.map(({ id, name, color }) => (
-            <Col key={id} xs={24} md={8} >
-              <Link to={`/categories/${id}`}>
-                <Card
-                  style={{
-                    cursor: 'pointer',
-                    textAlign: 'center',
-                    border: 'none',
-                    color: 'white',
-                    background: color,
-                    boxShadow: '0 4px 20px 0 rgb(0 0 0 / 14%), 0 7px 10px -5px rgb(76 175 80 / 40%)'
-                  }}
-                >
-                  <h1>{name}</h1>
-                </Card>
-              </Link>
-            </Col>
-          ))
-        }
-      </Row>
-    </Card>
+    <>
+      <Card style={{ marginBottom: '20px' }}>
+        <Typography>
+          <Title style={{
+            fontWeight: 200,
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px'
+          }}>
+            <AiOutlineUser /> ¡Bienvenido  {displayName ? displayName : email}!
+          </Title>
+          <Title style={{ marginTop: '20px' }}>
+            Intercorp Marketing Awards 2023
+          </Title >
+          <Paragraph>
+            ¡Bienvenido a Intercorp Marketing Awards! Aquí podrás postular tu caso de marketing más innovador y efectivo. Te invitamos a que lo presentes con todo detalle para que sea evaluado por nuestro jurado de expertos en el campo.
+          </Paragraph>
+          <Paragraph>
+            Nuestro proceso de evaluación consta de dos instancias de votación. En la primera, un grupo de expertos evaluará los casos y seleccionará a los finalistas. En la segunda, un jurado compuesto por líderes del sector decidirá quién gana el premio a la excelencia en marketing.
+          </Paragraph>
+        </Typography>
+      </Card>
+      <Card style={{ marginBottom: '20px' }}>
+        <CategoriesCollapse />
+      </Card>
+      <Card style={{ marginBottom: '20px' }}>
+        <Typography>
+          <Title style={{ margin: '8px 0 20px' }} level={4}>
+            Seleccione la categoría a postular
+          </Title>
+          <Paragraph>
+            Para ayudarte a clasificar tu propuesta, te pedimos que selecciones una o más de las siguientes categorías, solo se puede postular un caso por categoría.
+          </Paragraph>
+        </Typography>
+      </Card>
+      <Card>
+        <CategoriesCard />
+      </Card>
+    </>
   )
 }
