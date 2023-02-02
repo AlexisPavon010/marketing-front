e5f33400e583cd3c3216401d6b2fdf8558779364
Alexis Pavon <themaster034@gmail.com>
2023-02-02 18:19:02 -0300

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import './index.scss';
 import { AppRouter } from './routers/AppRouter';
 
 const root = ReactDOM.createRoot(
