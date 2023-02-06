ebf1fca6819e1035766fe3e56c47da862968a5f0
Alexis Pavon <themaster034@gmail.com>
2023-02-06 10:33:09 -0300

    add admin navbar

---
+++ src/components/Layout/AdminNavbar.tsx
@@ -0,0 +1,69 @@
+import { Row, Col, Button, Avatar, Layout, Dropdown } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import { CgMenu } from 'react-icons/cg';
+import type { MenuProps } from 'antd';
+
+import styles from './styles.module.scss'
+import { startLogout } from '../../store/auth';
+
+interface NavbarProps {
+  openMenu: () => any;
+}
+
+const { Header } = Layout;
+
+export const AdminNavbar = ({ openMenu }: NavbarProps) => {
+  const { displayName, photoURL, email } = useSelector((state: any) => state.auth)
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  const handleLogout = () => {
+    // @ts-ignore
+    dispatch(startLogout())
+  }
+
+  const items: MenuProps['items'] = [
+    {
+      label: <a href="https://www.antgroup.com">1st menu item</a>,
+      key: '0',
+    },
+    {
+      label: <a href="https://www.aliyun.com">2nd menu item</a>,
+      key: '1',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      label: 'Cerrar Sesión',
+      key: '3',
+      onClick: handleLogout
+    },
+  ];
+
+  return (
+    <Header style={{ background: 'white' }}>
+      <div className="header__wrapper">
+        <Row align="middle" gutter={16}>
+          <Col lg={0} >
+            <Button
+              style={{
+                display: 'flex'
+              }}
+              className="header__menu" type="link" onClick={openMenu} icon={<CgMenu size={24} color="#525F7F" />} />
+          </Col>
+          <Col flex={1}>
+          </Col>
+          <Col md={{ span: 'auto' }}>
+            <Dropdown menu={{ items }} trigger={['click']}>
+              <Avatar size='large' src={photoURL ? photoURL : null} alt={displayName} >
+                {displayName ? displayName?.charAt(0).toUpperCase() : email?.charAt(0).toUpperCase()}
+              </Avatar>
+            </Dropdown>
+          </Col>
+        </Row>
+      </div>
+    </Header>
+  )
+}
