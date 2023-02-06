80a08f614b8e70083deaf95a53fedb2a4bd8c196
Alexis Pavon <themaster034@gmail.com>
2023-02-06 10:40:14 -0300

    add private router

--- src/routers/PrivateRoute.tsx
+++ src/routers/PrivateRoute.tsx
@@ -13,6 +13,8 @@ export const PrivateRoute = ({ children }: PrivateRouteProps) => {
 
   if (!uid) {
     return <Navigate to="/login" />
+  } else if (pathname === '/dashboard' && role == 'user') {
+    return <Navigate to="/" />
   } else if (pathname === '/' && role == 'admin') {
     return <Navigate to="/dashboard" />
   }
