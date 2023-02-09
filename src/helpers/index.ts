5a66ada4cd27b5995c6da61de3106f4abf5153be
Alexis Pavon <themaster034@gmail.com>
2023-02-09 15:05:53 -0300

    remove firebase

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,7 @@
-import { doc, updateDoc } from "firebase/firestore/lite";
-
-import { FirebaseDB } from "../services/firebase";
 
 export const updateRole = async (docId: string, newRole: string) => {
-  const docRef = doc(FirebaseDB, "users", docId);
-  await updateDoc(docRef, {
-    role: newRole
-  })
+  // const docRef = doc(FirebaseDB, "users", docId);
+  // await updateDoc(docRef, {
+  //   role: newRole
+  // })
 }
