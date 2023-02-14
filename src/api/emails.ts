80989beaaf499e65ee42833e8cbdec8e4fcbc62a
Alexis Pavon <themaster034@gmail.com>
2023-02-14 11:53:56 -0300

    add welcome api

---
+++ src/api/emails.ts
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+export const sendWelcome = (email: string) => {
+  return axios.post(`${BASE_URL}/api/email`, {
+    email
+  })
+}
