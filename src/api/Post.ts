93aa7d13928e1e0e1568115d5f4bf497e6cb2ccf
Alexis Pavon <themaster034@gmail.com>
2023-02-17 16:40:59 -0300

    export base url

--- src/api/Post.ts
+++ src/api/Post.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
+export const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export const createPost = (payload: any) => {
   return axios.post(`${BASE_URL}/api/posts`, payload)
