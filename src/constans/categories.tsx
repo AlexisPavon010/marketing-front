4514a1c1b8b1544fd50e99fac854dca15a6f4b9d
Alexis Pavon <themaster034@gmail.com>
2023-02-17 16:41:49 -0300

    add categories

--- src/constans/categories.tsx
+++ src/constans/categories.tsx
@@ -1,5 +1,6 @@
 import { AiOutlinePlusSquare } from "react-icons/ai";
 import { BiChart } from "react-icons/bi";
+import { FiDatabase } from "react-icons/fi";
 import { HiOutlinePencilSquare } from "react-icons/hi2";
 import { MdOutlineBubbleChart, MdOutlineDraw, MdOutlineShowChart } from "react-icons/md";
 
@@ -38,6 +39,12 @@ export const CATEGORIES = [
     id: 'impacto-positivo',
     name: 'Impacto Positivo',
     icon: <AiOutlinePlusSquare size={32} />,
+    color: 'linear-gradient(45deg, rgba(18,122,222,1) 0%, rgba(66,162,255,1) 100%)'
+  },
+  {
+    id: 'uso-de-first-party-data',
+    name: 'Uso de First Party Data',
+    icon: <FiDatabase size={32} />,
     color: 'linear-gradient(45deg, rgba(18,122,222,1) 0%, rgba(66,162,255,1) 100%)'
   },
 ]
