aa59d2b0b654b0141b9de5d45ac608b94933b05f
Alexis Pavon <themaster034@gmail.com>
2023-03-02 14:38:22 -0300

    finish progess component

--- src/components/ProgressComponent/ProgressComponent.tsx
+++ src/components/ProgressComponent/ProgressComponent.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Progress } from "antd"
+import axios from "axios"
+
+import { BASE_URL } from "../../api/Post"
+import { IPost } from '../../interfaces/Post'
 
 export const ProgressComponent = () => {
+  const [pending, setPending] = useState([])
+  const total = 100 - (pending.length * 10)
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/api/posts`)
+      .then(({ data }) => {
+        setPending(data.posts.filter((post: IPost) => post.scored === false))
+      })
+      .catch()
+      .finally()
+  }, [])
+
   return (
-    <Progress percent={25} />
+    <Progress percent={total} />
   )
 }
