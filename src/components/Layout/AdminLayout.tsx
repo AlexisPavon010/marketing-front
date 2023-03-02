02c46d6daab4f88f2ca2df6e95e559e48166be1f
Alexis Pavon <themaster034@gmail.com>
2023-03-02 11:49:30 -0300

    remove side bar

--- src/components/Layout/AdminLayout.tsx
+++ src/components/Layout/AdminLayout.tsx
@@ -19,37 +19,37 @@ export const AdminLayout = ({ children }: Props) => {
 
   return (
     <>
-      <Layout className={styles.layout}>
+      {/* <Layout className={styles.layout}>
         <Sider className="sider" width={250} theme={'light'} >
           <div className={styles.sider__header} >
             <img className={styles.sider__logo} src="/assets/logo-v3.png" alt="" />
           </div>
           <SideMenu />
-        </Sider>
-        <Drawer
-          placement="left"
-          closable={false}
-          onClose={closeMenu}
-          open={isMenuOpen}
-          width={250}
-          bodyStyle={{
-            padding: 0,
-            position: 'fixed'
-          }}
-        >
-          <div className={styles.sider__header} >
-            <img className={styles.sider__logo} src="/assets/logo-v3.png" alt="" />
-          </div>
-          <SideMenu closeMenu={closeMenu} />
-        </Drawer>
-        <Layout>
-          <AdminNavbar openMenu={openMenu} />
-          <Content className={styles.main_content_admin}>
-            {children}
-          </Content>
-          <Footer />
-        </Layout>
+        </Sider> */}
+      <Drawer
+        placement="left"
+        closable={false}
+        onClose={closeMenu}
+        open={isMenuOpen}
+        width={250}
+        bodyStyle={{
+          padding: 0,
+          position: 'fixed'
+        }}
+      >
+        <div className={styles.sider__header} >
+          <img className={styles.sider__logo} src="/assets/logo-v3.png" alt="" />
+        </div>
+        <SideMenu closeMenu={closeMenu} />
+      </Drawer>
+      <Layout>
+        <AdminNavbar openMenu={openMenu} />
+        <Content className={styles.main_content_admin}>
+          {children}
+        </Content>
+        <Footer />
       </Layout>
+      {/* </Layout> */}
     </>
   )
 }
