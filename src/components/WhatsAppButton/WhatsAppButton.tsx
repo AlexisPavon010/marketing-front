2656656beee8ba25b214f2ee57cf9e44f2821b75
Alexis Pavon <themaster034@gmail.com>
2023-03-02 14:38:45 -0300

    change icon

--- src/components/WhatsAppButton/WhatsAppButton.tsx
+++ src/components/WhatsAppButton/WhatsAppButton.tsx
@@ -1,15 +1,14 @@
 import { Button } from "antd"
-import { BsWhatsapp } from "react-icons/bs"
+import { BsChatSquareDotsFill } from "react-icons/bs"
 
 import styles from './styles.module.scss'
-// 51942020546
 
 export const WhatsAppButton = () => {
   return (
     <Button
       className={styles.btn_whatsapp}
       onClick={() => window.open('https://api.whatsapp.com/send/?phone=5491123902334&type=phone_number&app_absent=0')}
-      icon={< BsWhatsapp size={24} />}
+      icon={< BsChatSquareDotsFill size={24} />}
       type="primary"
       size="large"
       shape="circle"
