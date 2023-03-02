e09e14e4c51f99d97bfdc16db24e5c98c2556883
Alexis Pavon <themaster034@gmail.com>
2023-03-02 11:51:11 -0300

    add component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,4 @@
 import { Card, Typography } from 'antd'
-import { AiOutlineUser } from 'react-icons/ai'
 import { useSelector } from 'react-redux'
 
 import styles from './home.module.scss'
@@ -10,35 +9,22 @@ import { StepsComponent } from '../../components/Steps'
 import { JuryCollapse } from '../../components/JuryCollapse'
 import { ProgressComponent } from '../../components/ProgressComponent'
 import { JuryForm } from '../../components/JuryForm'
+import { HomeComponent } from '../../components/HomeComponent'
+import { JuryComponent } from '../../components/JuryComponent'
 
 const { Title, Paragraph } = Typography;
 
 export const Home = () => {
-  const { username, email, role } = useSelector((state: any) => state.auth)
+  const { role } = useSelector((state: any) => state.auth)
 
   return (
     <>
       <Card className={styles.hero}>
-        <Typography>
-          <Title className={styles.hero__title} >
-            <AiOutlineUser /> {role === 'jury' ? `¡Bienvenido/a  ${username ? username : email}! al Jurado` : `¡Bienvenido/a  ${username ? username : email}!`}
-          </Title>
-          <Title className={styles.hero__subtitle}>
-            Intercorp Marketing Awards 2023
-          </Title >
-          <Paragraph>
-            Un premio que busca destacar a través de diferentes categorías de participación, a las acciones/campañas /ideas de marketing de las marcas de Intercorp que demuestran el mayor impacto en el cumplimiento de los objetivos de negocio.
-          </Paragraph>
-          <Paragraph>
-            En este documento van a encontrar los detalles de las categorías, algunos consejos para la presentación de casos, plazos de inscripción y todos los criterios de evaluación que es importante saber y tener en cuenta a la hora de aplicar.
-          </Paragraph>
-          <Paragraph>
-            Estamos orgullosos de lanzar por primera vez en Intercorp un premio de estas características.
-          </Paragraph>
-          <Paragraph>
-            ¡Buena suerte con tus inscripciones!
-          </Paragraph>
-        </Typography>
+        {role === 'jury' ? (
+          <JuryComponent />
+        ) : (
+          <HomeComponent />
+        )}
       </Card>
       <Card className={styles.card__categories}>
         <StepsComponent />
